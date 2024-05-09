fa9e2725773f8a777af67d396da70ce7faacaa66
Autcoder <florian.link06@icloud.com>
2024-05-09 18:05:07 +0200

    Checking Dependencies Function

--- quicklang-dnf/quicklang.nim
+++ quicklang-dnf/quicklang.nim
@@ -9,4 +9,38 @@ proc check_tar(): bool =
         echo "Tar is already installed"
         return true
 
-var installed = check_tar()
+proc check_make(): bool =
+    echo "Checking if make is installed..."
+    if execShellCmd("make --version") != 0:
+        echo "Make is not installed"
+        return false
+    else:
+        echo "Make is already installed"
+        return true
+
+proc check_curl(): bool =
+    echo "Checking if curl is installed..."
+    if execShellCmd("curl --version") != 0:
+        echo "Curl is not installed"
+        return false
+    else:
+        echo "Curl is already installed"
+        return true
+
+proc check_openjdk(): bool =
+    echo "Checking if Java is installed..."
+    if execShellCmd("java --version") != 0:
+        echo "Java is not installed"
+        return false
+    else:
+        echo "Java is already installed"
+        return true
+
+proc check_snap(): bool =
+    echo "Checking if snap is installed..."
+    if execShellCmd("snap --version") != 0:
+        echo "Snap is not installed"
+        return false
+    else:
+        echo "Snap is already installed"
+        return true
